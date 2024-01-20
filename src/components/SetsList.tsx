6d6787ff255280433dd21abe88c242c1d2b4e2af
Rokas <rookas.rudzenskas@gmail.com>
2024-01-20 13:17:19 +0200

    Fix

--- src/components/SetsList.tsx
+++ src/components/SetsList.tsx
@@ -1,30 +1,30 @@
 import { View, Text, ActivityIndicator, FlatList } from 'react-native';
-// import { gql } from 'graphql-request';
-// import { useQuery } from '@tanstack/react-query';
-// import graphqlClient from '../graphqlClient';
-//
-// const setsQuery = gql`
-//   query exercises {
-//     sets {
-//       documents {
-//         _id
-//         exercise
-//         reps
-//         weight
-//       }
-//     }
-//   }
-// `;
+import { gql } from 'graphql-request';
+import { useQuery } from '@tanstack/react-query';
+import graphqlClient from "@/src/components/graphqlClient";
+
+const setsQuery = gql`
+  query exercises {
+    sets {
+      documents {
+        _id
+        exercise
+        reps
+        weight
+      }
+    }
+  }
+`;
 
 const SetsList = () => {
-  // const { data, isLoading } = useQuery({
-  //   queryKey: ['sets'],
-  //   queryFn: () => graphqlClient.request(setsQuery),
-  // });
-  //
-  // if (isLoading) {
-  //   return <ActivityIndicator />;
-  // }
+  const { data, isLoading } = useQuery({
+    queryKey: ['sets'],
+    queryFn: () => graphqlClient.request(setsQuery),
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
 
   return (
     <FlatList
