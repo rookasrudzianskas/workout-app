3be2f7a9ef3393a7c21da84c29a84876df1fab8f
Rokas <rookas.rudzenskas@gmail.com>
2024-01-20 10:56:44 +0200

    fix

--- src/components/ExerciseListItem.tsx
+++ src/components/ExerciseListItem.tsx
@@ -4,10 +4,10 @@ import { Link } from 'expo-router';
 export default function ExerciseListItem({ item }: { item: any}) {
   return (
     <Link href={`/${item.name}`} asChild>
-      <Pressable style={styles.exerciseContainer}>
-        <Text style={styles.exerciseName}>{item.name}</Text>
+      <Pressable style={styles.exerciseContainer} className="bg-white p-3 rounded-md">
+        <Text className="font-semibold text-lg">{item.name}</Text>
 
-        <Text style={styles.exerciseSubtitle}>
+        <Text className="text-gray-500">
           <Text style={styles.subValue}>{item.muscle}</Text> |{' '}
           <Text style={styles.subValue}>{item.equipment}</Text>
         </Text>
@@ -18,13 +18,6 @@ export default function ExerciseListItem({ item }: { item: any}) {
 
 const styles = StyleSheet.create({
   exerciseContainer: {
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 10,
-    gap: 5,
-    marginHorizontal: 2,
-
-    // shadow
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -34,13 +27,6 @@ const styles = StyleSheet.create({
     shadowRadius: 1.41,
 
     elevation: 2,
-  },
-  exerciseName: {
-    fontSize: 20,
-    fontWeight: '500',
-  },
-  exerciseSubtitle: {
-    color: 'dimgray',
   },
   subValue: {
     textTransform: 'capitalize',
